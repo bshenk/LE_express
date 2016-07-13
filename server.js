37d90c593abbf7030246a1c4b4e2d852cb76b16f
bshenkd <bshenkd@gmail.com>
2016-07-13 14:24:19 -0400

    using port 443 as a HTTPS server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const https = require('https');
+
+const port = 443;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(80, () => {
-	console.log('Server started on port 80');
-});
+const certPath = '/etc/letsencrypt/live/axonai.ai/';
+const certKeyPath = certPath + 'privkey.pem';
+const certFilePath = certPath + 'cert.pem';
+
+const privateKey  = fs.readFileSync(certKeyPath, 'utf8');
+const certificate = fs.readFileSync(certFilePath, 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+const httpsServer = https.createServer(credentials, app);
+
+//httpServer.listen(8080);
+httpsServer.listen(port);
+
+console.log(`Server started on port: ${port}`);
